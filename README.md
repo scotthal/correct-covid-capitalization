224cf47d0fe8ceec1d71c52e6de68cbfc17131ec
Scott Hallock <scott@hallocks.net>
2020-06-25 08:57:56 -0600

    First working content script
    The content script replaces instances of "covid" with
    "COVID" everywhere on washingtonpost.com  Still to come
    is a UI that allows users to prefer that "covid" be
    replaced with "COVID".
    
    Known issue:  This will replace "covid" with "COVID" in
    scripts as well as in text rendered to the user.  This
    appears to be the state-of-the-art in text replacement
    Chrome extensions.  I intend to continue searching for
    a heuristic that reliably excludes script from
    replacement.
    
    Finally, there must be some way to avoid repeating myself
    in the manifest and still cover both http and https
    protocols for content script injection.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-External appearances suggest that it is the editorial policy of *The Washington
-Post* that the correct way to write, "COVID," is, "covid."
+External appearances suggest that it is the editorial policy of _The Washington
+Post_ that the correct way to write, "COVID," is, "covid."
 
 I respectfully disagree.
+
+As a bonus, this Chrome extension will correctly capitalize, "coronavirus," as,
+"Coronavirus," when you're reading _Washington Post_ articles.
---
+++ content.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const correctTextNode = (textNode) => {
+  textNode.textContent = textNode.textContent.replace(/covid/g, "COVID");
+  textNode.textContent = textNode.textContent.replace(
+    /coronavirus/g,
+    "Coronavirus"
+  );
+};
+
+const depthFirstTraversal = (node) => {
+  if (node.nodeType == Node.TEXT_NODE) {
+    correctTextNode(node);
+  }
+  node.childNodes.forEach((node) => depthFirstTraversal(node));
+};
+
+depthFirstTraversal(document);
